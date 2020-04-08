c3ad2348a62fcbed2d3ff1804b077fc8cc87788c
Nick <nikheal25@gmail.com>
2020-04-08 15:01:06 +1000

    created protocol file

---
+++ GreenFarmMonitor/UserDefaults/UserdefaultsController.swift
@@ -0,0 +1,13 @@
+//
+//  UserdefaultsController.swift
+//  GreenFarmMonitor
+//
+//  Created by Nikhil Gholap on 8/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import UIKit
+
+class UserdefaultsController: NSObject, UserdefaultsProtocol {
+
+}
---
+++ GreenFarmMonitor/UserDefaults/UserdefaultsProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  UserdefaultsProtocol.swift
+//  GreenFarmMonitor
+//
+//  Created by Nikhil Gholap on 8/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import Foundation
+
+protocol UserdefaultsProtocol {
+   
+}
