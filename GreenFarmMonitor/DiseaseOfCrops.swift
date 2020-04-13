b9545f8abf63d12c0fecc9748c4125955e4064c2
Hanlin Shen <hshe0017@student.monash.edu>
2020-04-13 20:25:37 +1000

    [Disease]

---
+++ GreenFarmMonitor/DiseaseOfCrops.swift
@@ -0,0 +1,9 @@
+//
+//  DiseaseOfCrops.swift
+//  GreenFarmMonitor
+//
+//  Created by Hanlin Shen on 13/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import Foundation
---
+++ GreenFarmMonitor/DiseaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DiseaseViewController.swift
+//  GreenFarmMonitor
+//
+//  Created by Hanlin Shen on 13/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import UIKit
+
+class DiseaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
