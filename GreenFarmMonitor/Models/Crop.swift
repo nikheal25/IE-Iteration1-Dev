5b9df78253d18d779e3a5df00baf2e355ce1c845
Nick <nikheal25@gmail.com>
2020-04-13 16:25:07 +1000

    added list

---
+++ GreenFarmMonitor/Models/Crop.swift
@@ -0,0 +1,15 @@
+//
+//  Crop.swift
+//  GreenFarmMonitor
+//
+//  Created by Nikhil Gholap on 13/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import Foundation
+
+class Crop: NSObject {
+    var cropId: String?
+    var cropName: String?
+    var cropImage: String?
+}
---
+++ GreenFarmMonitor/UserDefaults/CropTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CropTableViewCell.swift
+//  GreenFarmMonitor
+//
+//  Created by Nikhil Gholap on 13/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import UIKit
+
+class CropTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
