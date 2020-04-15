79eaf09da3b9a5a2b8adb66ef26da4b4f4009433
Nick <nikheal25@gmail.com>
2020-04-16 09:29:40 +1000

    daily check

---
+++ GreenFarmMonitor/DemoCell.swift
@@ -0,0 +1,24 @@
+//
+//  DemoCell.swift
+//  GreenFarmMonitor
+//
+//  Created by Nikhil Gholap on 15/4/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import UIKit
+
+class DemoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
