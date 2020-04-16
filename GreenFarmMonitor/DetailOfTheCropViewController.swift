9308fd7db04c4ec4e6133098a95808d4856f5a32
Nick <nikheal25@gmail.com>
2020-04-16 14:38:42 +1000

    added animation

--- GreenFarmMonitor/DetailOfTheCropViewController.swift
+++ GreenFarmMonitor/DetailOfTheCropViewController.swift
@@ -16,6 +16,11 @@ class DetailOfTheCropViewController: UIViewController {
     @IBOutlet weak var infoViewONE: UIView!
     @IBOutlet weak var infoViewTWO: UIView!
     
+    
+    @IBOutlet weak var temperatureBarView: UIView!
+    @IBOutlet weak var cropNameLabel: UILabel!
+    
+    @IBOutlet weak var tempLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         mainView.backgroundColor = UIColor(hexString: "#24343F")
@@ -23,8 +28,49 @@ class DetailOfTheCropViewController: UIViewController {
         self.imageView.layer.cornerRadius = 8
         self.imageView.layer.shadowOpacity = 0.4
         self.imageView.layer.shadowRadius = 1
+        
+        setInfoView()
+        
+        let shapeLayer = CAShapeLayer()
+       
+        let center = tempLabel.center
+        
+        let circularPath = UIBezierPath(arcCenter: center, radius: 40, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.fillColor = UIColor.white.cgColor
+        shapeLayer.lineWidth = 6
+        
+        shapeLayer.lineCap = CAShapeLayerLineCap.round
+        
+        shapeLayer.strokeEnd = 0
+        
+        temperatureBarView.layer.addSublayer(shapeLayer)
+        
+        let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        basicAnimation.toValue = 1
+        basicAnimation.duration = 3
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
+        
     }
     
+    func getPointForView(_ view : UIView) -> (x:CGFloat,y:CGFloat)
+    {
+        let x = view.frame.origin.x
+        let y = view.frame.origin.y
+        return (x,y)
+    }
+    
+    
+    func setInfoView()  {
+        self.infoView.layer.cornerRadius = 8
+        self.infoView.layer.shadowOpacity = 0.4
+        self.infoView.layer.shadowRadius = 1
+    }
     
     /*
      // MARK: - Navigation
