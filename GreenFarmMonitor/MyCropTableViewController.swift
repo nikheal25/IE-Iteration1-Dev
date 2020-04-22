67b5b7129a7ab40bcb42229669bbac8bbd79ff40
Nick <nikheal25@gmail.com>
2020-04-23 09:47:05 +1000

    final

--- GreenFarmMonitor/MyCropTableViewController.swift
+++ GreenFarmMonitor/MyCropTableViewController.swift
@@ -229,11 +229,11 @@ class MyCropTableViewController: UITableViewController, DatabaseListener {
 
     //This method gets called when any cell is selected by the user
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-          if indexPath.section == SECTION_ACTIVITY{
-              selectedRow = indexPath.row
-              tableView.deselectRow(at: indexPath, animated: true)
-              self.performSegue(withIdentifier: "foldingSegue", sender: self)
-          }
+//          if indexPath.section == SECTION_ACTIVITY{
+//              selectedRow = indexPath.row
+//              tableView.deselectRow(at: indexPath, animated: true)
+//              self.performSegue(withIdentifier: "foldingSegue", sender: self)
+//          }
       }
   
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
