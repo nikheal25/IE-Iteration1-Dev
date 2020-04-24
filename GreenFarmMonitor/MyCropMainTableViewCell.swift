7c729504e724b905af9623b90c651e3c9ea7f41a
Nick <nikheal25@gmail.com>
2020-04-24 15:10:08 +1000

    my crop - UI changes

--- GreenFarmMonitor/MyCropMainTableViewCell.swift
+++ GreenFarmMonitor/MyCropMainTableViewCell.swift
@@ -12,6 +12,7 @@ class MyCropMainTableViewCell: UITableViewCell {
 
     @IBOutlet weak var cropTitleLable: UILabel!
     @IBOutlet weak var imageTitle: UIImageView!
+    @IBOutlet weak var cellView: UIView!
     
     func setCell(crop: Crop) {
             cropTitleLable.text = crop.cropName
@@ -21,9 +22,28 @@ class MyCropMainTableViewCell: UITableViewCell {
 //        self.layer.cornerRadius = 10
 //        self.layer.shadowOpacity = 5
 //        self.layer.shadowRadius = 10
-        imageTitle.layer.cornerRadius = 10
-        cropTitleLable.textColor = .black
+//        imageTitle.layer.cornerRadius = 10
+//        cropTitleLable.textColor = .black
+//
+//              //// MARK:- color behind cell
+////                self.contentView.backgroundColor = UIColor(hexString: "#B5D4BE")
+//                self.layer.cornerRadius = 20
+//                self.layer.shadowOpacity = 4
+//                self.layer.shadowRadius = 2
+        //        self.cellView.layer.masksToBounds = true
+        //        self.cellView.layer.shadowColor = UIColor(named: "Orange")?.cgColor
+        //         self.cropImageView.layer.shadowOffset = CGSize(width: 3, height: 3)
+        
         self.imageTitle.image = UIImage(named: crop.cropImage)
+        
+         self.contentView.backgroundColor = UIColor(hexString: "#B5D4BE")
+                self.cellView.layer.cornerRadius = 8
+                self.cellView.layer.shadowOpacity = 0.4
+                self.cellView.layer.shadowRadius = 2
+        //        self.cellView.layer.masksToBounds = true
+        //        self.cellView.layer.shadowColor = UIColor(named: "Orange")?.cgColor
+        //         self.cropImageView.layer.shadowOffset = CGSize(width: 3, height: 3)
+                self.imageTitle.layer.cornerRadius = 6
         
         //TODO
 //        self.cropTitleLable.layer.cornerRadius = 10
