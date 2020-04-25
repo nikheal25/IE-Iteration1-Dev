f6fc7a6b40291492ab46dad8e912835ba09d4301
Nick <nikheal25@gmail.com>
2020-04-25 15:33:51 +1000

    UI changes

--- GreenFarmMonitor/GrowMyCropTableViewCell.swift
+++ GreenFarmMonitor/GrowMyCropTableViewCell.swift
@@ -20,8 +20,9 @@ class GrowMyCropTableViewCell: UITableViewCell {
     
     @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var cropTitleLable: UILabel!
-    @IBOutlet weak var growCropButton: UIButton!
     @IBOutlet weak var cropImageView: UIImageView!
+    @IBOutlet weak var arrowImage: UIImageView!
+    
     func setCell(crop: Crop) {
         self.crop = crop
         //        cropImageView = image
@@ -29,9 +30,9 @@ class GrowMyCropTableViewCell: UITableViewCell {
         
         cropImageView.image = UIImage(named: crop.cropImage)
         
-        self.growCropButton.layer.cornerRadius = 3
-        self.growCropButton.layer.borderWidth = 1
-        self.growCropButton.layer.borderColor = UIColor.black.cgColor
+//        self.growCropButton.layer.cornerRadius = 3
+//        self.growCropButton.layer.borderWidth = 1
+//        self.growCropButton.layer.borderColor = UIColor.black.cgColor
        
         self.contentView.backgroundColor = UIColor(hexString: "#3A4F41")
         self.cellView.layer.cornerRadius = 8
@@ -39,13 +40,25 @@ class GrowMyCropTableViewCell: UITableViewCell {
         self.cellView.layer.shadowRadius = 2
 
         self.cropImageView.layer.cornerRadius = 6
+        
+        
+        //MARK:- make image clickable
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(GrowMyCropTableViewCell.imageTapped(gesture:)))
+
+        // add it to the image view;
+        arrowImage.addGestureRecognizer(tapGesture)
+        // make sure imageView can be interacted with by user
+        arrowImage.isUserInteractionEnabled = true
+        
     }
     
-    @IBAction func growCropButtonClicked(_ sender: Any) {
+    @objc func imageTapped(gesture: UIGestureRecognizer) {
+        // if the tapped view is a UIImageView then set it to imageview
         if(self.delegate != nil){ //Just to be safe.
-                   self.delegate.callSegueFromCell(crop: self.crop)
-               }
+            self.delegate.callSegueFromCell(crop: self.crop)
+        }
     }
+            
     override func setSelected(_ selected: Bool, animated: Bool) {
        
     }
