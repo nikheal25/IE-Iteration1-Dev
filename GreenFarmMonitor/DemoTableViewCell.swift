a3e3e85669d98e1224d0a8274099d8ee45686b23
Nick <nikheal25@gmail.com>
2020-04-26 10:03:47 +1000

    UI fix: changed the label

--- GreenFarmMonitor/DemoTableViewCell.swift
+++ GreenFarmMonitor/DemoTableViewCell.swift
@@ -48,6 +48,7 @@ class DemoTableViewCell: FoldingCell {
         self.topBarLabel.text = crop.cropName
         self.descriptionLabel.adjustsFontSizeToFitWidth = true
         self.descriptionLabel.minimumScaleFactor = 0.5
+        self.descriptionLabel.sizeToFit()
         self.descriptionLabel.text = crop.Description
         
         
