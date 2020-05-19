987c530364f28c248762d088f9f300c0cb1e7467
hanlin <hshe0017@student.monash.edu>
2020-05-19 20:08:30 +1000

    relevantResults

---
+++ GreenFarmMonitor/relevantLocationsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  relevantLocationsTableViewCell.swift
+//  GreenFarmMonitor
+//
+//  Created by Hanlin Shen on 19/5/20.
+//  Copyright © 2020 Nikhil Gholap. All rights reserved.
+//
+
+import UIKit
+
+class relevantLocationsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var searchResult: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
